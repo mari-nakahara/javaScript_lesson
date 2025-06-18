d918cf5678503d89cf3c51454a4cc62d41c75ae3
中原麻理 <withusnakahara@gmal.com>
2025-06-18 18:39:43 +0900

    課題提出

--- practice/app.js
+++ practice/app.js
@@ -1,4 +1,15 @@
-let numVal = 8;
-let strVal = 'text'
+let mixed = [4, '2', 5, '8', '9', 0, 1];
 
-console.log(numVal === 7 || strVal === 'text');
+for (let i = 0; i < mixed.length; i++) {
+  let item = mixed[i];
+
+  if (typeof item === 'number') {
+    if (item % 2 === 0) {
+      console.log('even');
+    } else {
+      console.log('odd');
+    }
+  } else {
+    console.log('not number');
+  }
+}
